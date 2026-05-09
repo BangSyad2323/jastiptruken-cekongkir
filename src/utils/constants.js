b070c5db64f1310c13a76d3ab934add92631cb7f
17:45:39 <nurildani81@gmail.com>
2026-05-09 16:54:20 +0700

    harga 2000

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const ADMIN_LOCATION = { lat: -6.963013, lng: 110.146176 };
 
 export const PRICING_CONFIG = {
   PICKUP_FEE_PER_KM: 1000,    // Admin ke Toko
-  DELIVERY_FEE_PER_KM: 3000,  // Toko ke Customer
+  DELIVERY_FEE_PER_KM: 2000,  // Toko ke Customer
   FIXED_JASTIP_FEE: 2000,    // Fee Jastip Tetap
 };
 
